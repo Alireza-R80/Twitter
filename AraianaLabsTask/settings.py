ea5d8a5d8e7192426837ecfd487b46f69253b3ce
arezaei <mr.alirezarezaei.1380@gmail.com>
2023-06-10 03:30:20 -0400

    Add channle settings

--- AraianaLabsTask/settings.py
+++ AraianaLabsTask/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -102,6 +103,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'AraianaLabsTask.wsgi.application'
+
+#Daphne
+ASGI_APPLICATION = "AraianaLabsTask.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
