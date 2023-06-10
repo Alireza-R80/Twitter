705b4d968cda7759998d9dac7e6008542f332669
arezaei <mr.alirezarezaei.1380@gmail.com>
2023-06-10 06:49:46 -0400

    consumer bug fixed.

--- tweets/consumer.py
+++ tweets/consumer.py
@@ -11,9 +11,8 @@ class TweetConsumer(WebsocketConsumer):
         # Add the user to a specific WebSocket group based on their followers
         self.user = self.get_user_from_jwt()
 
-        if self.user:
+        if self.user is not None:
             self.followers_group_name = f"followers_{self.user.id}"
-            print(self.user)
             async_to_sync(self.channel_layer.group_add)(
                 self.followers_group_name, self.channel_name
             )
@@ -24,23 +23,21 @@ class TweetConsumer(WebsocketConsumer):
 
 
     def disconnect(self, close_code):
-        # Remove the user from the WebSocket group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.followers_group_name, self.channel_name
-        )
+        if self.user:
+            # Remove the user from the WebSocket group
+            async_to_sync(self.channel_layer.group_discard)(
+                self.followers_group_name, self.channel_name
+            )
+        
 
     def get_user_from_jwt(self):
         jwt_auth = JWTAuthentication()
         try:
             # Get the token from the headers
             query_params = parse_qs(self.scope['query_string'].decode())
-            print(query_params)
-            print("------------------------------------------")
             token = query_params.get('token')[0]
-            print(token)
             validated_token = jwt_auth.get_validated_token(token)
             user = jwt_auth.get_user(validated_token)
-            print(user)
             return user
         except Exception:
             return None
